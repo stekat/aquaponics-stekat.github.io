24a4b8389e51855832abce64caf68c1d85d1b668
Dietsch <sdietsch@saxoprint.local>
2018-09-10 15:48:38 +0200

    Instafeed aktualisiert

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var instaFeed = new Instafeed({
     get: 'user',
       limit: 5,
       userId: 6222370876,
+    //   clientId: 'b2bd864e6f19412da5d29dcfae7cd587',
       accessToken: '6222370876.2e9310474d6f4e93a2203de39f105615',
       resolution: 'thumbnail',
       template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>',
